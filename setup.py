33021472cb8275a190f681166a85be1515052ead
jennie <168314850+1amjennie261@users.noreply.github.com>
2024-05-13 11:22:30 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='assignment',
+    version='1.0',
+    description='.',
+    author='Jen',
+    author_email='1amjennie261@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+      
+    ],
+)
